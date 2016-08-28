79a14c7c3c86dc3d7736a00397f31346fdc0c19f
planetdata <amitaa.singh@gmail.com>
2016-08-28 16:50:12 +1000

    final commit

--- power.r
+++ power.r
@@ -59,7 +59,11 @@ dim(pow)    #[1] 2880   10
 str(pow)
 
 pow$Global_active_power <- as.numeric(pow$Global_active_power)
- 
+pow$Sub_metering_1<-as.numeric(pow$Sub_metering_1)
+pow$Sub_metering_2<-as.numeric(pow$Sub_metering_2)
+pow$Sub_metering_3<-as.numeric(pow$Sub_metering_3)
+pow$Volatage<-as.numeric(pow$Voltage)
+
 
 # HISTOGRAM
 
@@ -85,15 +89,6 @@ ggtitle("Global Active Power") + xlab("Global Active Power (kilowatts)") + ylab(
 datetime<- strptime(paste(pow$Date, pow$Time, sep=" "), "%Y-%m-%d %H:%M:%S") 
 pow$datetime<-datetime
 plot(datetime, pow$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
-
-
-
-
-pow$Sub_metering_1<-as.numeric(pow$Sub_metering_1)
-pow$Sub_metering_2<-as.numeric(pow$Sub_metering_2)
-pow$Sub_metering_3<-as.numeric(pow$Sub_metering_3)
-
-
 plot(datetime,pow$Sub_metering_1,type="l",ylab="Energy Submetering",xlab="")
 lines(datetime,pow$Sub_metering_2,type="l",col="red")
 lines(datetime,pow$Sub_metering_3,type="l",col="green")
@@ -105,10 +100,6 @@ legend("topright",c("sub_metering_1","sub_metering_2","sub_metering_3"),lty=1,lw
 # layouts 
 
 par(mfrow=c(2,2)) # 4 plots in one layout
-
-pow$Volatage<-is.numeric(pow$Voltage)
-
-
 #1.
 plot(datetime,pow$Global_active_power,type="l",ylab="Global Active Power",xlab="")
 
@@ -119,7 +110,7 @@ plot(datetime,pow$Voltage,type='l',ylab="Voltage",xlab="datetime")
 plot(datetime,pow$Sub_metering_1,type="l",ylab="Energy sub metering",xlab="")
 lines(datetime,pow$Sub_metering_2,type="l",col="red")
 lines(datetime,pow$Sub_metering_3,type="l",col="blue")
-legend("topright",c("sub_metering_1","sub_metering_2","sub_metering_3"),lty=1,lwd=2.5,cex=0.5,col=c("black","red","blue"))
+legend("topright",c("sub_metering_1","sub_metering_2","sub_metering_3"),lty=1,lwd=2.5,cex=0.25,col=c("black","red","blue"))
 
 #4.
 plot(datetime,pow$Global_reactive_power,type="l",xlab="datetime",ylab="Global_Reactive_Power")
