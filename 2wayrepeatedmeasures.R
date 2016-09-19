9e24dda4a1ff1d3e7fd4b050021866478fb2f37b
planetdata <amitaa.singh@gmail.com>
2016-09-19 16:26:57 +1000

    Update 2wayrepeatedmeasures.R

--- 2wayrepeatedmeasures.R
+++ 2wayrepeatedmeasures.R
@@ -3,24 +3,14 @@ diet <- read_excel(path,col_names=F)
 View(diet)
 names(diet) <- c("subject","exercise","intensity1","intensity2","intensity3","diet")
 # 
-# each group has 6 subjects. 
-# we are interested to find out the response  pulse rate of each of the subjects
-# independent variables  : diet, exercise
-# So far, two-factor ANOVA with 3 replicates per combo of exercise- type and diet
-# BUT
-#But: 3 responses for each subject: pulse rate at low, moderate, high intensity in their exercise.
-# Makes it repeated measures.
-#3 factors:
-#  ???diet and exercise type "between subjects",
-# ???intensity-of-exercise "within subjects".
 
 
-diet<- diet[order(diet$subject),]
+#diet<- diet[order(diet$subject),]
 #diet1<- select(diet,2:6)
-require(dplyr)
-View(diet)
+#require(dplyr)
+#View(diet)
 #diet1$subject<- 1:nrow(diet1)
-str(diet)
+#str(diet)
 
 diet$exercise<- factor(diet$exercise)
 diet$diet<- factor(diet$diet)
@@ -37,12 +27,20 @@ View(diet.long)
 mod <- aov(value ~ diet*exercise*intensity + Error(subject/intensity) , data=diet.long)
 summary(mod)
 
+
+
+
+
+
+
 mean_pulse<-with(diet.long,tapply(value,list(diet,intensity),mean))
 mp <- stack(as.data.frame(mean_pulse))
 mp$diet <- rep(seq_len(nrow(mean_pulse)),ncol(mean_pulse))
 mp$diet<-factor(mp$diet,labels = c("Meat","Veg"))
-ggplot(mp,aes(ind,values,group=diet,color=diet)) + geom_line() + xlab("Intensity of the exercise") +
-  ylab("Mean Pulse Rate") + ggtitle("Mean Pulse rate - \n Exercise Intensity vs Diet")
+
+
+#ggplot(mp,aes(ind,values,group=diet,color=diet)) + geom_line() + xlab("Intensity of the exercise") +
+#  ylab("Mean Pulse Rate") + ggtitle("Mean Pulse rate - \n Exercise Intensity vs Diet")
 
 
 
@@ -57,3 +55,32 @@ mp1$Exercise<-factor(mp1$Exercise)
 ggplot(mp1,aes(Intensity,values,group=Diet,color=Diet)) + geom_line(lwd=1) + xlab("Intensity of the exercise") +
   ylab("Mean Pulse Rate") + ggtitle("Mean Pulse rate - \n Exercise Intensity vs Diet") + theme_grey()+
  facet_grid(Exercise ~.)
+
+#An interaction plot is a visual representation of the interaction between the effects of two factors,
+# or between a factor and a numeric variable. It is suitable for experimental data.
+#If one of the explanatory variables is numeric and the other is a factor, list the numeric variable first and the factor second. This way the
+#numeric variable is displayed along the x-axis and the factor is represented by separate lines on the plot.
+
+interaction.plot(mp1$Intensity, mp1$Diet, mp1$values , type="b", col=c("red","blue"), legend=F,
+                 lwd=2, pch=c(18,24),
+                 xlab="Exertion intensity", 
+                 ylab="Mean pulse rate ", 
+                 main="Interaction Plot")
+
+
+
+legend("bottomright",c("Meat","Veg"),bty="n",lty=c(1,2),lwd=2,pch=c(18,24),col=c("red","blue"),title="Diet")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
